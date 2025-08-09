15d9308106d7631fe099c4c1283fc45c71ee3a2f
HIEITOM <hiei-tom@z-bs.co.jp>
2025-08-10 00:06:31 +0900

    ストレージのパスを修正し、ユーザープロフィール画像のアップロード先をpublic/profile-imagesに変更

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'profileImages',
   access: (allow) => ({
-    'profile-images/*': [
+    'public/profile-images/*': [
       allow.authenticated.to(['read', 'write', 'delete']),
       allow.guest.to(['read'])
     ]
